d2cf71aa29ae9cd480b8928415b6c91a7ff0088f
unknown <ayxan689@gmail.com>
2023-12-05 17:52:02 +0400

    Pagination added

--- main.js
+++ main.js
@@ -2,41 +2,48 @@ document.addEventListener("DOMContentLoaded", function () {
     const productListElement = document.getElementById("productList");
     const searchInput = document.getElementById("searchInput");
     const categoryFilter = document.getElementById("categoryFilter");
+    const pageSize = 10; 
 
     let products;
+    let currentPage = 1;
 
-    const apiUrl = "https://dummyjson.com/products";
+    const apiUrl = "https://dummyjson.com/products?limit=100";
 
     fetch(apiUrl)
         .then(response => {
             if (!response.ok) {
-                throw new Error(`Network response was not ok: ${response.status}`);
+                throw new Error(`Network response error: ${response.status}`);
             }
             return response.json();
         })
         .then(data => {
             products = data.products;
-            populateCategoryFilter(products);
-            displayProducts(products);
+            generateCategoryOptions(products);
+            renderProducts(products, currentPage);
+            establishPagination(products);
         })
         .catch(error => {
-            console.error('Error fetching data:', error);
+            console.error('Error:', error);
         });
 
-    function displayProducts(products) {
+    function renderProducts(products, page) {
+        const startIndex = (page - 1) * pageSize;
+        const endIndex = startIndex + pageSize;
+        const productsToDisplay = products.slice(startIndex, endIndex);
+
         productListElement.innerHTML = "";
 
-        if (!Array.isArray(products)) {
-            console.error('Invalid data format:', products);
+        if (!Array.isArray(productsToDisplay)) {
+            console.error('Wrong data format:', productsToDisplay);
             return;
         }
 
-        if (products.length === 0) {
+        if (productsToDisplay.length === 0) {
             console.warn('No products found.');
             return;
         }
 
-        products.forEach(product => {
+        productsToDisplay.forEach(product => {
             const productCard = document.createElement("div");
             productCard.classList.add("product-card");
 
@@ -54,7 +61,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    function populateCategoryFilter(products) {
+    function generateCategoryOptions(products) {
         const categories = [...new Set(products.map(product => product.category))];
         categories.forEach(category => {
             const option = document.createElement("option");
@@ -72,7 +79,10 @@ document.addEventListener("DOMContentLoaded", function () {
         fetch(`https://dummyjson.com/products/search?q=${searchTerm}`)
             .then(response => response.json())
             .then(data => {
-                displayProducts(data.products);
+                products = data.products;
+                currentPage = 1;
+                renderProducts(products, currentPage);
+                establishPagination(products);
             })
             .catch(error => {
                 console.error('Error fetching search results:', error);
@@ -84,10 +94,52 @@ document.addEventListener("DOMContentLoaded", function () {
         fetch(`https://dummyjson.com/products/category/${selectedCategory}`)
             .then(response => response.json())
             .then(data => {
-                displayProducts(data.products);
+                products = data.products;
+                currentPage = 1;
+                renderProducts(products, currentPage);
+                establishPagination(products);
             })
             .catch(error => {
                 console.error('Error fetching category results:', error);
             });
+    }
+
+    function establishPagination(products) {
+        const totalPages = Math.ceil(products.length / pageSize);
+    
+        const existingPaginationContainer = document.querySelector(".pagination");
+        if (existingPaginationContainer) {
+            existingPaginationContainer.remove();
+        }
+    
+        const paginationContainer = document.createElement("div");
+        paginationContainer.classList.add("pagination");
+    
+        const backwardButton = createPaginationButton("<", () => {
+            if (currentPage > 1) {
+                currentPage--;
+                renderProducts(products, currentPage);
+               
+            }
+        });
+        paginationContainer.appendChild(backwardButton);
+    
+        const forwardButton = createPaginationButton(">", () => {
+            if (currentPage < totalPages) {
+                currentPage++;
+                renderProducts(products, currentPage);
+            }
+        });
+        paginationContainer.appendChild(forwardButton);
+    
+        document.body.appendChild(paginationContainer);
+    
+        }
+
+    function createPaginationButton(label, clickHandler) {
+        const button = document.createElement("button");
+        button.textContent = label;
+        button.addEventListener("click", clickHandler);
+        return button;
     }
 });
